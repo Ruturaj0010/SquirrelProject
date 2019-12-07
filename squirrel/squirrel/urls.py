a7dec9fc12b0128e8e0f329aa08566333d928d6e
Tejas <tg2718@columbia.edu>
2019-12-07 20:14:52 +0000

    Add url to urls

--- squirrel/squirrel/urls.py
+++ squirrel/squirrel/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlstrend = [
+     path('squirrel_sightings/', include('Squirrel_Sightings.urls')),   
+     path('admin/', admin.site.urls),
+ ]
