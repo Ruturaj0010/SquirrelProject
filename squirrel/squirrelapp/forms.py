ce175605302d4b7ab1d4c2fe013006496ed28b3c
Tejas <tg2718@columbia.edu>
2019-12-10 04:35:31 +0000

    added forms & views

---
+++ squirrel/squirrelapp/forms.py
@@ -0,0 +1,12 @@
+
+m django.forms import ModelForm
+from .models import Squirrels
+from django.db import models
+from django import forms
+class SquirrelForm(ModelForm):
+        
+       Specific_location = forms.CharField(required=False)
+       Other_Activities = forms.CharField(required=False)
+       class Meta:
+           model = Squirrels
+           fields = '__all__'
--- squirrel/squirrelapp/views.py
+++ squirrel/squirrelapp/views.py
@@ -77,7 +77,64 @@ def stats(request):
             chasing_count +=1
         else:
             pass
+        if F.Primary_Fur_Colour == 'Black':
+            black_count +=1
+        elif F.Primary_Fur_Colour == 'Cinnamon':
+            cinnamon_count +=1
+        elif F.Primary_Fur_Colour == 'Gray':
+            gray_count +=1
+        else:
+            pass
+        if F.Foraging == True:
+            foraging_count +=1
+        else:
+            pass
+        if F.Eating == True:
+            eating_count +=1
+        else:
+            pass
+        if F.Age == 'Juvenile':
+            juvenile_count +=1
+        elif F.Age == 'Adult':
+            adult_count +=1
+        else:
+            pass
+        if F.Kuks == True:
+            kuks_count +=1
+        else:
+            pass
+        if F.Quaas == True:
+            quaas_count +=1
+        else:
+            pass
+        if F.Moans == True:
+            moans_count +=1
+        else:
+            pass
+        if F.Tail_flags == True:
+            tail_flags_count +=1
+        else:
+            pass
+     details = {'Squirrels found eating': eating_count,
+                 'Squirrels found running':running_count,
+                 'Sqquirrels found climbing':climbing_count,
+                 'Squirrels found chasing':chasing_count,
+                 'Squirrels found with black fur':black_count,
+                 'Squirrels found with cinnamon fur':cinnamon_count,
+                 'Squirrels found with gray fur':gray_count,
+                 'Squirrels found foraging':foraging_count,
+                 'Squirrels found flagging tails':tail_flags_count,
+                 'Squirrels found as a juvenile':juvenile_count,
+                 'Squirrels found as a adult': adult_count,
+                 'Squirrels found making kuk sounds':kuks_count,
+                 'Squirrels found making quaas sounds':quaas_count,
+                 'Squirrels found making moan sounds': moans_count,
+               }
+     context = {"stats":details}
+     return render(request, 'squirrelapp/stats.html', context)
 
+
+        
 
 
 
