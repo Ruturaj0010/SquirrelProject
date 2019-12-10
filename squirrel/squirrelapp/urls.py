ac858a47e73b89033ed93ac8ebe30208b9e87188
Ruturaj <rrb2151@columbia.edu>
2019-12-10 08:29:13 +0000

    squirrel id error fix

--- squirrel/squirrelapp/urls.py
+++ squirrel/squirrelapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
                 path('',views.home),
                 path('map/',views.coordinates),
                 path('add/', views.add_squirrel),
-                path('<str:Unique_Squirrel_ID>/edit/',views.edit_squirrel),
+                path('views/<str:Squirrel_Id>/edit/',views.edit_squirrel),
                 path('stats/',views.stats),
 ]
